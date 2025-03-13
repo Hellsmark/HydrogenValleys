bf008f0fd86ad5bf8c45614940c6248c1c5eef2f
jakobwesterback <jakwes@chalmers.se>
2025-03-13 18:52:37 +0100

    small errors fix

--- h2_projects/Contact_info_scraping/contact_info_scrape.R
+++ h2_projects/Contact_info_scraping/contact_info_scrape.R
@@ -6,7 +6,6 @@ library(dplyr)
 library(ggplot2)
 library(stringr)
 
-
 OPENAI_API_KEY <- Sys.getenv("OPENAI_API_KEY")
 
 ss <- "https://docs.google.com/spreadsheets/d/1xzpre5Ej_7OEGRU4EA7KZuMQnSz5YCyTx5Sdbml6bQE/edit?gid=0#gid=0"
@@ -34,22 +33,23 @@ output_structure <- type_array(
 results<- list()
 
 #insert every text in chat_gpt and store results as lists in "results"
-for (i in 1:20){
+for (i in 20:40){
   extracted_data <- chat$extract_data(
-    sum$description[i],
+    small_df$description[i],
     type=output_structure)
-  extracted_data$company<- sum$company[i]
+  extracted_data$company<- small_df$company[i]
   results[[i]]<- extracted_data
 }
 
 clean_contacts <- function(results, df) {
   final_df <- bind_rows(results) %>%  #Combine all the results into a final df.
     filter(!name %in% df$company)%>%  # Remove name if it's a company name
-    filter(!str_detect(tolower(title), "recruiter|rekryterande chef|rekryterare|recruitment contact")) %>%  # Filter some titles
-    mutate(name = str_remove(name, "^(Prof\\.|Docent)\\s+")) %>%  # Remove titles from names
+    filter(!str_detect(tolower(title), "recruiting chef|recruiter|recruitment manager|recruiting manager|rekryterande chef|rekryterare|recruitment contact")) %>%  # Filter some titles
+    mutate(name = str_remove(name, "^(Prof\\.|Docent|Assistant Professor)\\s+"))%>% #removes titles in front of name
     distinct(name, .keep_all = TRUE) %>%  # Remove duplicates
     
-  return(final_df)
+    return(final_df)
 }
 
-clean<- clean_contacts(results, df)
+cleans<- clean_contacts(results, df)
+
