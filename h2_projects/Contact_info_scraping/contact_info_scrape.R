7435b0baa2c8ae760ffc3329b7d22811907405f1
jakobwesterback <jakwes@chalmers.se>
2025-04-06 18:37:12 +0200

    Final code for exctracting information from description

--- h2_projects/Contact_info_scraping/contact_info_scrape.R
+++ h2_projects/Contact_info_scraping/contact_info_scrape.R
@@ -16,10 +16,6 @@ df <- df_raw %>% janitor::clean_names()
 small_df <- df%>% select(company,description)
 
 
-#prompt
-chat<- chat_openai(
-  system_prompt = "Extract contact information to the people mentioned in the 
-  texts i show you. Create a row for each person mentioned in the text.")
 
 #Define what data points we want to extract
 output_structure <- type_array(
@@ -33,22 +29,27 @@ output_structure <- type_array(
 results<- list()
 
 #insert every text in chat_gpt and store results as lists in "results"
-for (i in 15:25){
+for (i in 1:20){
+  chat<- chat_openai(
+    system_prompt = "Extract contact information to the people mentioned in the 
+  texts i show you. Create a row for each person mentioned in the text.")
+  Sys.sleep(3)
+  
   if (nchar(small_df$description[i]) <= 12000){ #Don't read adds if they are too long
     extracted_data <- chat$extract_data(
       small_df$description[i],
       type=output_structure)
   extracted_data$company<- small_df$company[i]
   results[[i]]<- extracted_data
+  print(i)
 }}
 
 clean_contacts <- function(results, df) {
   final_df <- bind_rows(results) %>%  #Combine all the results into a final df.
     filter(!name %in% df$company)%>%  # Remove name if it's a company name
-    filter(!str_detect(tolower(title), "recruiting chef|recruiter|recruitment manager|recruiting manager|rekryterande chef|rekryterare|recruitment contact")) %>%  # Filter some titles
+    #filter(!str_detect(tolower(title), "recruiting chef|recruiter|recruitment manager|recruiting manager|rekryterande chef|rekryterare|recruitment contact")) %>%  # Filter some titles
     mutate(name = str_remove(name, "^(Prof\\.|Docent|Assistant Professor)\\s+"))%>% #removes titles in front of name
     distinct(name, .keep_all = TRUE) %>%  # Remove duplicates
-    
     return(final_df)
 }
 
